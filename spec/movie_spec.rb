ed696fd58b81c8bfa5c5a0d74a4d4e9f258f9e04
john <kiokoj81@gmail.com>
2023-01-16 18:44:31 +0300

    Create class movie and tests for it

---
+++ spec/movie_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/movie'
+
+describe 'Movie Class' do
+  before(:each) do
+    @movie = Movie.new('2022-25-12')
+  end
+  it 'returns true' do
+    expect(@movie.silent).to eql false
+  end
+  it 'returns true' do
+    expect(@movie.move_to_archive).to eql false
+  end
+end
